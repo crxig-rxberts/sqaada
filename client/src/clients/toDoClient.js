863a2424ae792c05c3ca6fd922c2c331d5ab983f
Craig Roberts <craig.roberts@rs-components.com>
2024-07-10 13:37:42 +0100

    fix integration tests

--- client/src/clients/toDoClient.js
+++ client/src/clients/toDoClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api';
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8080/api';
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 # Get the server IP
 SERVER_IP=$(curl -s http://checkip.amazonaws.com)
 
-# Export it as an environment variable
 export SERVER_IP=$SERVER_IP
+export NODE_ENV='production'
 
 # Start the application
 node server/index.js
