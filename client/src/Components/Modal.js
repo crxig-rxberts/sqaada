2f6a71d8843b17b533d1b836b2b18121379f004d
alexander.keen <alexander.keen@sykescottages.co.uk>
2024-07-14 16:38:08 +0100

    refactor for list details component

---
+++ client/src/Components/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Modal = ({ isOpen, onClose, title, children }) => {
+    if (!isOpen) return null;
+
+    return (
+        <>
+            <div className="modal d-block align-content-center" tabIndex="-1" role="dialog">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{title}</h5>
+                            <button type="button" className="btn-close" onClick={onClose} aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            {children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal-backdrop fade show"></div>
+        </>
+    );
+};
+
+export default Modal;
